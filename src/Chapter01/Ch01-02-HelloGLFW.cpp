bdceff4d1ca7036a5fd5792815a9a1b6b5f2b450
Mr.Moon <MrMoonKr@gmail.com>
2025-01-31 19:07:07 +0900

    GLAD, GLFW 라이브러리 테스트 ...

---
+++ src/Chapter01/Ch01-02-HelloGLFW.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main( int argc, char** argv )
+{
+    std::cout << "Hello, GLFW!, 안녕, 지엘에프더블유" << std::endl;
+
+    if ( GLFW_TRUE != glfwInit() )
+    {
+        std::cout << "failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Initialize GLAD to load OpenGL functions
+    if ( 0 != gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress ) )
+    {
+        std::cout << "failed to initialize GLAD " << std::endl;
+        return -1;
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
